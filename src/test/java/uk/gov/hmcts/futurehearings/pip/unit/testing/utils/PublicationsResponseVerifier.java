71a1ee2ed2939ff50d219a2d6433df33ed8db118
mjotichmcts <milan.jotic1@hmcts.net>
2021-06-27 14:06:49 +0100

    initial tests for publications

---
+++ src/test/java/uk/gov/hmcts/futurehearings/pip/unit/testing/utils/PublicationsResponseVerifier.java
@@ -0,0 +1,28 @@
+package uk.gov.hmcts.futurehearings.pip.unit.testing.utils;
+
+import io.restassured.response.Response;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static uk.gov.hmcts.futurehearings.pip.unit.testing.utils.TestReporter.getObjStep;
+
+public class PublicationsResponseVerifier {
+
+    public static void thenValidateResponseForGetPublications(Response response){
+        try{
+            Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
+            assertEquals(200, response.getStatusCode(),"Status Code Validation:");
+            getObjStep().pass("Got the expected status code: 200");
+        }
+        catch (AssertionError e){
+            getObjStep().fail("Exception in "+e.getMessage());
+            throw e;
+        }
+        catch (Exception e){
+            getObjStep().fail("Exception: "+e.getClass());
+            throw e;
+        }
+    }
+
+}

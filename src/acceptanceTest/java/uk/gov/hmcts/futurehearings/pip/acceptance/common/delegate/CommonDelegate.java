32ee2b6313517f7d13d716226e18e3e146bcbde6
ZhangS <shufen.zhang@HMCTS.net>
2021-07-08 11:00:06 +0100

    Tidy up formats/style

--- src/acceptanceTest/java/uk/gov/hmcts/futurehearings/pip/acceptance/common/delegate/CommonDelegate.java
+++ src/acceptanceTest/java/uk/gov/hmcts/futurehearings/pip/acceptance/common/delegate/CommonDelegate.java
@@ -8,13 +8,13 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 
 public interface CommonDelegate {
-    public Response test_expected_response_for_supplied_header(final String authorizationToken,
-                                                               final String targetURL,
-                                                               final String inputFile,
-                                                               final Map<String, String> standardHeaderMap,
-                                                               final Headers headers,
-                                                               final Map<String, String> params,
-                                                               final HttpMethod httpMethod,
-                                                               final HttpStatus status);
+    Response test_expected_response_for_supplied_header(final String authorizationToken,
+                                                        final String targetURL,
+                                                        final String inputFile,
+                                                        final Map<String, String> standardHeaderMap,
+                                                        final Headers headers,
+                                                        final Map<String, String> params,
+                                                        final HttpMethod httpMethod,
+                                                        final HttpStatus status);
 
 }

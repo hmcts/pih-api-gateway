dc2f3465eb274058e2cafa63f572ddddea65c232
Milan Jotic <milan.jotic@beo-tech.com>
2021-07-14 14:38:28 +0100

    added iimports

--- src/test/java/uk/gov/hmcts/futurehearings/pip/unit/testing/testsuites/GetPipHealthCheck.java
+++ src/test/java/uk/gov/hmcts/futurehearings/pip/unit/testing/testsuites/GetPipHealthCheck.java
@@ -2,6 +2,8 @@ package uk.gov.hmcts.futurehearings.pip.unit.testing.testsuites;
 
 import io.restassured.response.Response;
 import lombok.extern.slf4j.Slf4j;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;

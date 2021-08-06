259a177f868b6fc82979aee96b2c653c85705b6c
Shufen Zhang <85679190+ShufenZ@users.noreply.github.com>
2021-08-06 10:22:52 +0100

    Added smoke test for health check.Removed redundant parameter from the template for returning the response (#19)

--- src/smokeTest/java/uk/gov/hmcts/futurehearings/pip/smoke/common/rest/RestClientTemplate.java
+++ src/smokeTest/java/uk/gov/hmcts/futurehearings/pip/smoke/common/rest/RestClientTemplate.java
@@ -8,11 +8,9 @@ import static io.restassured.RestAssured.given;
 public class RestClientTemplate {
 
     public static Response makeGetRequest(final Map<String, String> headers,
-                                          final String authorizationToken,
                                           final String rootContext) {
         return given()
             .headers(headers)
-            .auth().oauth2(authorizationToken)
             .basePath(rootContext)
             .when().get();
     }

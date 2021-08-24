7bb3874017f37e60493334c5146dc9f9e2ccdd91
Christopher Pateman <christopher.pateman@version1.com>
2021-08-24 11:37:58 +0100

    VIH-7898 upgrade tf and correct true switch

--- infrastructure/terraform/00-init.tf
+++ infrastructure/terraform/00-init.tf
@@ -1,9 +1,17 @@
 terraform {
-  required_version = ">= 0.12.0"
-  backend "azurerm" {}
+  backend "azurerm" {
+  }
+
+  required_version = ">= 0.15.4"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.7.0"
+    }
+  }
 }
 
+
 provider "azurerm" {
-  version = "2.42.0"
   features {}
 }
